6b5f79ea01578611050971459851d1399f4e9f43
Ian Link <ian.link@luckyirishmedia.com>
2026-05-06 10:25:37 -0400

    Add top banner with brand color #7800C2

---
+++ components/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <div className="bg-[#7800C2] text-white text-center py-2 text-sm font-medium">
+      <p>Welcome to Purple Fireflies</p>
+    </div>
+  )
+}
