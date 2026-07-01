25b6d8500eb26774ea3843ef76e30deeea0efc8e
Ian Link <ian.link@luckyirishmedia.com>
2026-07-01 14:46:43 -0400

    Rename KV binding from CACHE to PURPLE_FIREFLIES_KV

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -1,6 +1,6 @@
 interface CloudflareEnv {
   purple_fireflies_db: D1Database;
-  CACHE: KVNamespace;
+  PURPLE_FIREFLIES_KV: KVNamespace;
   EMAIL_API_KEY?: string;
   EMAIL_FROM?: string;
   EMAIL_API_URL?: string;
